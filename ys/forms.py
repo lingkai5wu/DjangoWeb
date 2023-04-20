08c0397a23aa6e4fd8ed4e892b919dd726e2d538
lingkai5wu <lingkai5wu@outlook.com>
2023-04-20 15:03:57 +0800

    refactor: 重构更新代码
    将更新业务的实现移动到 forms.py

--- ys/forms.py
+++ ys/forms.py
@@ -1,10 +1,37 @@
+import requests
 from django import forms
+
+from ys.models import Content
 
 
 class UpdateContentForm(forms.Form):
     page_size = forms.IntegerField(label='pageSize', min_value=1)
     page_num = forms.IntegerField(label='pageNum', min_value=1)
     channel_id = forms.IntegerField(label='channelId')
+
+    def get_content_list(self):
+        page_size = self.cleaned_data['page_size']
+        page_num = self.cleaned_data['page_num']
+        channel_id = self.cleaned_data['channel_id']
+        url = 'https://content-static.mihoyo.com/content/ysCn/getContentList'
+        payload = {'pageSize': page_size, 'pageNum': page_num, 'channelId': channel_id}
+        data = requests.get(url, params=payload).json()
+        if data['retcode'] == 0:
+            content_list = data['data']['list']
+            total = data['data']['total']
+            return content_list, total
+        else:
+            raise ValueError(data['message'])
+
+    @staticmethod
+    def update_content(content):
+        obj, created = Content.objects.update_or_create(
+            content_id=content['contentId'],
+            defaults={'content_id': content['contentId'],
+                      'title': content['title'],
+                      'start_time': content['start_time'] + '+08:00'}
+        )
+        return f'{"添加" if created else "覆盖"}\t{obj.title}'
 
 
 class SelectContentForm(forms.Form):
--- ys/views.py
+++ ys/views.py
@@ -1,4 +1,3 @@
-import requests
 from django.db.models import Q
 from django.http import HttpResponse
 from django.urls import reverse
@@ -11,49 +10,23 @@ from ys.forms import UpdateContentForm, SelectContentForm
 from ys.models import Content, Update
 
 
-def get_content_list(page_size, page_num, channel_id):
-    url = 'https://content-static.mihoyo.com/content/ysCn/getContentList'
-    payload = {'pageSize': page_size, 'pageNum': page_num, 'channelId': channel_id}
-    data = requests.get(url, params=payload).json()
-    if data['retcode'] == 0:
-        content_list = data['data']['list']
-        total = data['data']['total']
-        return content_list, total
-    else:
-        raise ValueError(data['message'])
-
-
-def update_content(content):
-    obj, created = Content.objects.update_or_create(
-        content_id=content['contentId'],
-        defaults={'content_id': content['contentId'],
-                  'title': content['title'],
-                  'start_time': content['start_time'] + '+08:00'}
-    )
-    return f'添加\t{obj.title}' if created else f'覆盖\t{obj.title}'
-
-
 class UpdateContentView(FormView):
     template_name = 'update.html'
     form_class = UpdateContentForm
-    success_url = '/update/'
 
     def form_valid(self, form):
-        page_size = form.cleaned_data['page_size']
-        page_num = form.cleaned_data['page_num']
-        channel_id = form.cleaned_data['channel_id']
         try:
-            content_list, total = get_content_list(page_size, page_num, channel_id)
+            content_list, total = form.get_content_list()
         except ValueError as e:
             return HttpResponse(str(e))
         context = self.get_context_data()
         context['total'] = total
-        context['list'] = [update_content(content) for content in content_list]
+        context['list'] = [form.update_content(content) for content in content_list]
         Update.objects.create(update_time=timezone.now(), total=total)
         return self.render_to_response(context)
 
     def get_initial(self):
-        return {'page_num': 1, 'channel_id': 10}
+        return {'page_size': 20, 'page_num': 1, 'channel_id': 10}
 
 
 class UpdateInfoMixin(ContextMixin):
